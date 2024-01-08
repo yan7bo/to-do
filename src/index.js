0b2426a788286e437ed2a67f7fec43551e8f6593
Bo Yan <yan7bo@gmail.com>
2024-01-08 17:56:51 -0500

    Adds Project as an object

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
+// Project
+// object has:
+// - name
+// - description
+// - list of Task objects
+// - due date for the whole Project
+
+// Task
+// object has:
+// - name
+// - description
+// - due date for the specific task
+
 function List() {
     const projects = [];
 
+    function Project(name = "project", tasks = [], date = "2024-12-31", description = "sample") {
+        this.name = name;
+        this.tasks = tasks;
+        this.date = date,
+        this.description = description;
+    }
+
     const promptProject = () => {
+        // returns a Project object
         const name = prompt("Name of project:");
         console.log(`project: ${name}`);
-        return name;
+        const newProject = new Project(name);
+        return newProject;
     }
 
     const addProject = () => {
